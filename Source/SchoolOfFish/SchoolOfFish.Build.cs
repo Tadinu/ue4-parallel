a45ba1c45969614d843c3caab6abf54a07600a7e
duc <duc@ascent.ai>
2021-06-28 13:10:16 +0900

    Revert Chaos dependency adding

--- Source/SchoolOfFish/SchoolOfFish.Build.cs
+++ Source/SchoolOfFish/SchoolOfFish.Build.cs
@@ -9,12 +9,9 @@ public class SchoolOfFish : ModuleRules
         bEnableExceptions = true;
         CppStandard = CppStandardVersion.Cpp17;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Projects", "RHI", "Chaos", "IntelISPC", "ShaderFishPlugin" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Projects", "RHI", "IntelISPC", "ShaderFishPlugin" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Projects", "RHI", "Chaos", "IntelISPC", "ShaderFishPlugin" });
-        
-        SetupModulePhysicsSupport(Target);
-        PrivateDefinitions.Add("CHAOS_INCLUDE_LEVEL_1=1");
+		PrivateDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Projects", "RHI", "IntelISPC", "ShaderFishPlugin" });
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
